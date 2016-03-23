ddcc84c688bb702451cec054893fe99770987688
dlaudio <admin@danlismore.com>
2016-03-23 12:08:26 +0000

    Initial Commit

---
+++ SecSock/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SecSock
+//
+//  Created by dtl on 23/03/2016.
+//  Copyright (c) 2016 LiosMor Security. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
